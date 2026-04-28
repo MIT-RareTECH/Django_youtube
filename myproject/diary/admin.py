00785804f85b42e180916f7e8762a382d3e0492e
MIT-RareTECH <mitsuki.swimmer@gmail.com>
2026-04-28 23:56:53 +0900

    45分まで

--- myproject/diary/admin.py
+++ myproject/diary/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Page
 
-# Register your models here.
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ["id", "create_at", "update_at"]
